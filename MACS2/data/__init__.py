9952481b4714b036ef0b2e979faa533596cbc627
Tao Liu <vladimir.liu@gmail.com>
2012-02-11 16:24:16 -0500

    Add a missing file.

---
+++ MACS2/data/__init__.py
@@ -0,0 +1,31 @@
+available_c = ['0.05','0.01','0.005','0.001']
+
+from array import array as pyarray
+import os
+
+class InvalidCError(Exception):
+    """Exception about invalid C.
+
+    e.g.:
+
+    raise InvalidCError(0.6)
+    """
+    def __init__ (self, c):
+        self.c = str(c)
+        
+    def __str__ (self):
+        return repr( "Specified C is not available: %s; Should be %s" % (self.strand, "\,".join(available_c)) )
+
+class PreCompiledGFold:
+    def __init__ ( self, c ):
+        path = os.path.dirname(__file__)
+        c = str(c)
+        if c in available_c:
+            self.gfolds = pyarray('f',[])
+            self.gfolds.fromfile(file(os.path.join(path,'g'+str(c)+'.dat'),'rb'),10201) # 10201=101*101
+        else:
+            raise InvalidCError(c)
+    
+    def get ( self, v1, v2 ):
+        i = int(round(v1)*101+round(v2))
+        return self.gfolds[i]
