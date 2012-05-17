4ad9f0dd7e32d7845a8ab34f4faa8137442c408f
Benjamin Schiller <ben.j.schiller@gmail.com>
2012-05-17 16:24:41 -0700

    cleanup code in cSignal.py

--- MACS2/cSignal.pyx
+++ MACS2/cSignal.pyx
@@ -6,6 +6,9 @@ from math import sqrt as mathsqrt
 
 cpdef maxima(np.ndarray[np.float32_t, ndim=1] signal,
              int window_size=51):
+    """return the local maxima in a signal after applying a 2nd order
+    Savitsky-Golay (polynomial) filter using window_size specified  
+    """
     cdef np.ndarray[np.int32_t, ndim=1] m = np.where(np.diff(np.sign(savitzky_golay_order2(signal, window_size, deriv=1))) == 2)[0].astype('int32')
     return m
 
@@ -32,6 +35,20 @@ cdef inline float sqrt(float threshold):
     
 cpdef enforce_peakyness(np.ndarray[np.float32_t, ndim=1] signal,
                         np.ndarray[np.int32_t, ndim=1] maxima):
+    """requires peaks described by a signal and a set of points where the signal
+    is at a maximum to meet a certain set of criteria
+    
+    maxima which do not meet the required criteria are discarded
+    
+    criteria:
+        for each peak:
+            calculate a threshold of the maximum of its adjacent two minima
+                plus the sqrt of that value
+            subtract the threshold from the region bounded by those minima
+            clip that region if negative values occur inside it
+            require it be > 50 bp in width
+            require that it not be too flat (< 6 unique values) 
+    """
     cdef:
         np.ndarray[np.int32_t, ndim=1] minima = internal_minima(signal, maxima)
         np.ndarray[np.float32_t, ndim=1] new_signal
@@ -77,10 +94,15 @@ cdef bool is_valid_peak(np.ndarray[np.float32_t, ndim=1] signal, int maximum):
 
 # require at least 6 different float values -- prevents broad flat peaks
 cdef bool too_flat(np.ndarray[np.float32_t, ndim=1] signal):
+#    """return whether signal has at least 6 unique values
+#    """
     return np.unique(signal).shape[0] < 6
 
 # hard clip a region with negative values
 cdef np.ndarray[np.float32_t, ndim=1] hard_clip(np.ndarray[np.float32_t, ndim=1] signal, int maximum):
+#    """clip the signal in both directions at the nearest values <= 0
+#    to position maximum
+#    """
     cdef:
         int i
         int left = 0
@@ -127,6 +149,9 @@ cdef np.ndarray[np.float32_t, ndim=1] hard_clip(np.ndarray[np.float32_t, ndim=1]
 cpdef enforce_valleys(np.ndarray[np.float32_t, ndim=1] signal,
                       np.ndarray[np.int32_t, ndim=1] summits,
                       float min_valley = 0.8):
+    """require a value of <= min_valley * lower summit
+    between each pair of summits
+    """
     cdef:
         float req_min, v, prev_v
         int summit_pos, prev_summit_pos
@@ -159,56 +184,42 @@ cpdef enforce_valleys(np.ndarray[np.float32_t, ndim=1] signal,
 # switched to double precision for internal accuracy
 cpdef savitzky_golay_order2(np.ndarray[np.float32_t, ndim=1] signal,
                      int window_size, int deriv=0):
-#    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-#    The Savitzky-Golay filter removes high frequency noise from data.
-#    It has the advantage of preserving the original shape and
-#    features of the signal better than other types of filtering
-#    approaches, such as moving averages techhniques.
-#    Parameters
-#    ----------
-#    y : array_like, shape (N,)
-#        the values of the time history of the signal.
-#    window_size : int
-#        the length of the window. Must be an odd integer number.
-#    order : int
-#        the order of the polynomial used in the filtering.
-#        Must be less then `window_size` - 1.
-#    deriv: int
-#        the order of the derivative to compute (default = 0 means only smoothing)
-#    Returns
-#    -------
-#    ys : ndarray, shape (N)
-#        the smoothed signal (or it's n-th derivative).
-#    Notes
-#    -----
-#    The Savitzky-Golay is a type of low-pass filter, particularly
-#    suited for smoothing noisy data. The main idea behind this
-#    approach is to make for each point a least-square fit with a
-#    polynomial of high order over a odd-sized window centered at
-#    the point.
-#    Examples
-#    --------
-#    t = np.linspace(-4, 4, 500)
-#    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
-#    ysg = savitzky_golay(y, window_size=31, order=4)
-#    import matplotlib.pyplot as plt
-#    plt.plot(t, y, label='Noisy signal')
-#    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
-#    plt.plot(t, ysg, 'r', label='Filtered signal')
-#    plt.legend()
-#    plt.show()
-#    References
-#    ----------
-#    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
-#       Data by Simplified Least Squares Procedures. Analytical
-#       Chemistry, 1964, 36 (8), pp 1627-1639.
-#    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
-#       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
-#       Cambridge University Press ISBN-13: 9780521880688
-#    """
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techhniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+
+    References
+    ----------
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
     cdef:
         int half_window, k
-        # precompute coefficients
         np.ndarray[np.int64_t, ndim=2] b
         # pad the signal at the extremes with
         # values taken from the signal itself
