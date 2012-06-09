03e4976f59ace0ed823132ddd65f36b997af59bc
Benjamin Schiller <ben.j.schiller@gmail.com>
2012-06-08 17:05:25 -0700

    *correct portability fix. should work with distutils sans cython

--- setup.py
+++ setup.py
@@ -53,7 +53,9 @@ def main():
                    Extension("MACS2.IO.cBedGraph", ["MACS2/IO/cBedGraph.c"], libraries=["m"]),
                    Extension("MACS2.IO.cScoreTrack", ["MACS2/IO/cScoreTrack.c"], include_dirs=numpy_include_dir ),
                    Extension("MACS2.IO.cCompositeScoreTrack", ["MACS2/IO/cCompositeScoreTrack.c"],),
-                   Extension("MACS2.hashtable", ["MACS2/hashtable.c", "MACS2/khash.h"],include_dirs=["MACS2/",numpy_get_include()]),
+                   Extension("MACS2.hashtable", ["MACS2/hashtable.c"],
+                             depends = ["MACS2/khash.h"],
+                             include_dirs=["MACS2/",numpy_get_include()]),
                    ]
     
     setup(name="MACS",
