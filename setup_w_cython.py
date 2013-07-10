20407db167f083ca93240d4b7e400bc5e029a224
Tao Liu <vladimir.liu@gmail.com>
2013-07-10 12:56:36 -0400

    label 2.0.10.20130710: with fixes for output format.

--- setup_w_cython.py
+++ setup_w_cython.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Time-stamp: <2013-05-28 00:28:14 Tao Liu>
+# Time-stamp: <2013-07-10 12:56:09 Tao Liu>
 
 """Description: 
 
@@ -80,7 +80,7 @@ def main():
                        ]
 
     setup(name="MACS",
-          version="2.0.10.20130528",
+          version="2.0.10.20130710",
           description="Model Based Analysis for ChIP-Seq data",
           author='Tao Liu',
           author_email='vladimir.liu@gmail.com',
