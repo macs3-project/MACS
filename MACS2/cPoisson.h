0326966db295efc36de6cae0030579ba0f2498d9
Tao Liu <vladimir.liu@gmail.com>
2013-10-10 01:31:57 -0400

    missing header file.

---
+++ MACS2/cPoisson.h
@@ -0,0 +1,4 @@
+double log10_poisson_cdf ( unsigned int n, double lam, short lower );
+double log10_poisson_cdf_P_large_lambda ( unsigned int k, double lbd );
+double log10_poisson_cdf_Q_large_lambda ( unsigned int k, double lbd );
+
