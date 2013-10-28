e1060b5183e5846e5884424c1ed274e3f9e3703c
Tao Liu <vladimir.liu@gmail.com>
2013-10-28 11:30:28 -0400

    Testing scripts.

--- test/test.sh
+++ test/test.sh
@@ -5,3 +5,68 @@ macs2 refinepeak -b random_test_CS_summits.bed -i random_test.bed -o random_test
 bdg2bw random_test_CS_treat_pileup.bdg hg19.len
 bedtools sort -i random_test.bed | bedtools bedtobam -i - -g hg19.len > random_test.bam
 samtools index random_test.bam
+
+
+# test all sub-commands
+
+CHIP=ChIP_200K.bed.gz
+CTRL=Control_200K.bed.gz
+
+# callpeak
+
+macs2 callpeak -t $CHIP -c $CTRL -n run_callpeak_narrow -B --outdir run_callpeak_narrow
+macs2 callpeak -t $CHIP -c $CTRL -n run_callpeak_broad -B --outdir run_callpeak_broad --broad
+
+# bdgcmp
+
+macs2 bdgcmp -t run_callpeak_narrow/run_callpeak_narrow_treat_pileup.bdg -c run_callpeak_narrow/run_callpeak_narrow_control_lambda.bdg -m ppois FE --outdir run_bdgcmp_multiple -o run_bdgcmp_myMulti
+
+macs2 bdgcmp -t run_callpeak_narrow/run_callpeak_narrow_treat_pileup.bdg -c run_callpeak_narrow/run_callpeak_narrow_control_lambda.bdg -m FE --outdir run_bdgcmp_single --ofile run_bdgcmp_single_myFE.bdg
+
+# bdgpeakcall
+
+macs2 bdgpeakcall -i run_bdgcmp_single/run_bdgcmp_single_myFE.bdg -c 2 --outdir run_bdgpeakcall -o run_bdgpeakcall_w_prefix
+
+macs2 bdgpeakcall -i run_bdgcmp_single/run_bdgcmp_single_myFE.bdg -c 2 --outdir run_bdgpeakcall --ofile run_bdgpeakcall_w_ofile.narrowPeak
+
+# bdgbroadcall
+
+macs2 bdgbroadcall -i run_bdgcmp_single/run_bdgcmp_single_myFE.bdg -c 4 -C 2 --outdir run_bdgbroadcall -o run_bdgbroadcall_w_prefix
+
+macs2 bdgbroadcall -i run_bdgcmp_single/run_bdgcmp_single_myFE.bdg -c 4 -C 2 --outdir run_bdgbroadcall --ofile run_bdgbroadcall_w_ofile.gappedPeak
+
+# diffpeak
+
+#NA
+
+# bdgdiff
+
+macs2 callpeak -c $CHIP -t $CTRL -n run_callpeak_narrow_revert -B --outdir run_callpeak_narrow_revert
+
+macs2 bdgdiff --t1 run_callpeak_narrow/run_callpeak_narrow_treat_pileup.bdg --c1 run_callpeak_narrow/run_callpeak_narrow_control_lambda.bdg --t2 run_callpeak_narrow_revert/run_callpeak_narrow_revert_treat_pileup.bdg --c2 run_callpeak_narrow_revert/run_callpeak_narrow_revert_control_lambda.bdg -o run_bdgdiff_prefix --outdir run_bdgdiff
+
+macs2 bdgdiff --t1 run_callpeak_narrow/run_callpeak_narrow_treat_pileup.bdg --c1 run_callpeak_narrow/run_callpeak_narrow_control_lambda.bdg --t2 run_callpeak_narrow_revert/run_callpeak_narrow_revert_treat_pileup.bdg --c2 run_callpeak_narrow_revert/run_callpeak_narrow_revert_control_lambda.bdg --ofile cond1.bed cond2.bed common.bed --outdir run_bdgdiff
+
+# filterdup
+
+macs2 filterdup -i $CHIP --outdir run_filterdup -o run_filterdup_result.bed
+
+# predictd
+
+macs2 predictd -i $CHIP --outdir run_predictd --rfile run_predictd.R
+
+# pileup
+
+macs2 pileup -i $CHIP --outdir run_pileup -o run_pileup.bdg
+
+# randsample
+
+macs2 randsample -t $CHIP -n 100000 --seed 31415926 --outdir run_randsample -o run_randsample.bed
+
+# refinepeak
+
+macs2 refinepeak -b run_callpeak_narrow/run_callpeak_narrow_peaks.narrowPeak -i $CHIP --outdir run_refinepeak -o run_refinepeak_w_prefix
+
+macs2 refinepeak -b run_callpeak_narrow/run_callpeak_narrow_peaks.narrowPeak -i $CHIP --outdir run_refinepeak --ofile run_refinepeak_w_ofile.bed
+
+
