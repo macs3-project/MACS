87fca3ec38f8e01cd6838b709145654255949f69
Tao Liu <vladimir.liu@gmail.com>
2015-01-21 10:16:23 -0500

    Change version number.

--- setup_w_cython.py
+++ setup_w_cython.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Time-stamp: <2014-10-29 02:00:22 Tao Liu>
+# Time-stamp: <2015-01-21 10:15:54 Tao Liu>
 
 """Description: 
 
@@ -82,7 +82,7 @@ def main():
                        ]
 
     setup(name="MACS2",
-          version="2.1.0.20141030",
+          version="2.1.0.20150120",
           description="Model Based Analysis for ChIP-Seq data",
           author='Tao Liu',
           author_email='vladimir.liu@gmail.com',
