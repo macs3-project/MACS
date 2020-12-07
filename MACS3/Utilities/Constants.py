978febcf9cc13eaebdbc9e9366855dff282c19a0
Tao Liu <vladimir.liu@gmail.com>
2020-12-06 23:25:39 -0500

    use buffer to read input file every 10M bytes

--- MACS3/Utilities/Constants.py
+++ MACS3/Utilities/Constants.py
@@ -3,4 +3,5 @@ MAX_PAIRNUM = 1000
 MAX_LAMBDA  = 100000
 FESTEP      = 20
 BUFFER_SIZE = 100000                   # np array will increase at step of 1 million items
-
+READ_BUFFER_SIZE = 10000000            # 10M bytes for read buffer size
+N_MP = 2                               # Number of processers
