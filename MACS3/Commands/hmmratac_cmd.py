ccf0e811717aa0428ef89e79078a36cb2828f578
philippadoherty <94635687+philippadoherty@users.noreply.github.com>
2022-01-07 17:07:07 -0500

    updated hmmratac_cmd outline

--- MACS3/Commands/hmmratac_cmd.py
+++ MACS3/Commands/hmmratac_cmd.py
@@ -97,16 +97,19 @@ def run( args ):
 
     # print version number ... do we want to list a HMMRATAC version number?
 
-    # print arguments entered
-
-    # read in genome size stats ... is this necessary
+    # read in genome size stats ... is this necessary?
+    gReader = GenomeFileReader(genomeFile)
+    genomeStats = gReader.getMap
 
     # read in blacklisted if option entered
     if blacklist:
-        info("Read in blacklisted")
+        black = bedFileReader(blacklist)
+        info("Read in blacklisted...")
         #bedFileReader, getData are used in java file
 
     # set fragment length distribution parameters use input values to set initial values, if provided. else use defaults
+    #  do we need to include these, since defaults are entered in macs3 file?
+    mode = [0.5, 2, 2, 2]
     if em_means != [50, 200, 400, 600]:
         info("Input values used for initial mean values for the fragment distribution...")
     else:
@@ -119,9 +122,12 @@ def run( args ):
     # Pull the lengths from the read data. Fragment distribution uses fragments with length > 100 to train the nucleosome distributions. 
     # the short distribution is set to 50 and remains unchanged. Only occurs if EM training occurs, else use the default settings
     if em_skip == False:
+
+        puller = pullLargeLengths(bamfile, index, minMapQ, genomeStats, em_means)
+        lengths = puller.getSampledLengths
+        weights = puller.getWeights
         # Perform EM training:
-        # pullLargeLengths(bam, index, minMapQ, genomeStats, em_means), length = getSampledLengths, weights = getWeights used in java script
-        # HMMR_EM(weights, em_means, em_stddev, length)
+        em = HMMR_EM(weights, em_means, em_stddev, lengths)
         info("Fragment Expectation Maximum performed on the fragment distribution...")
     else:
         info("EM training NOT performed on the fragment distribution...")
@@ -129,43 +135,144 @@ def run( args ):
 
 	# Generate genome wide pileup of read coverage and simultaneously calculate mean and standard deviation to calculate fold change and zscore. Convert pileup to bedgraph for easier storage. Calculate the pileup in chunks for memory efficiency
     # NOTE: this method does not currently exist. Instead we use an inputed big wig file to find fold change training regions and zscored masked regions
-	    # various functions used here:
-        # pileup, SplitBed(genomeStats, vitWindow, bam, index, rmdup), bedGraphMath(pileupData.getBedGraph)
-        # pileupData.getCPMScale, genomeMean, genomeStd, MergeBed(upper, lower)
-        # set train size, choose list with randomTrainSeed
+    pileupData = pileup(SplitBed(genomeStats, window), bamfile, index, keep_dups)
+    fc = bedGraphmath(pileupData.getBedgraph)
+    cpmScale = pileupData.getCPMScale/1000000
+    info("Scaling Factor = %s", cpmScale)
+    genomeMean = fc.getMean
+    genomeStd = fc.getStd
+    
+    train = MergeBed(fc.getBetweenRanges(upper, lower))
+    
+    if len(train) < maxTrain:
+        maxTrain = len(train)
+
+    # Shuffle training list before choosing (add randomTrainSeed here)
+    newTrain = []
+    for i in range(0, maxTrain):
+        newTrain.append(train[i])
+    
+    train = newTrain
+    train = ExtendBed(train)
+    exclude = MergeBed(fc.getAboveZscore(zscore))
+    addback = exclude
+
+    if blacklist:
+        exclude.addAll(black)
+        exclude = MergeBed(exclude)
+
+    if print_ex:
+        ex = "excluded regions to print"
+
+    for i in range(0, len(train)):
+        counter = 0
+        node1 = train[i]
+        for j in range(0, len(exclude)):
+            node2 = exclude[j]
+            if SubtractedBed.overlap(node1, node2):
+                counter += 1
+        if counter == 0:
+            newTrain.append(node1)
+
+    if train_regions:
+        trainReader = bedFileReader(train_regions)
+        train = trainReader()
     info("Training regions found and Zscore regions for exclusions found...")
 
     # Create the fragment pileup tracks using the training set and the fragment distribution parameters
+    # Use input model if available
     if model:
+        hmm = HMMBinaryReader(model)
         info("Binary model file used (generated from previous HMMR run), training skipped... ")
     else:
         # print output to _training.bed
-        # FragPileupGen, holder = TrackHolder
+        gen = FragPileupGen(bamfile, index, train, mode, em_means, em_stddev, minMapQ, keep_dups, cpmScale)
+        holder = TrackHolder(gen, trim)
         info("Training Fragment Pileup completed...")
     
         # Create the inital model using KMeans and then refine it using Baum-Welch
-        KMeansToHMM(holder)
+        kmeans = KMeansToHMM(holder,states)
         info("Kmeans Model: ...")
-
-    # Use input model if available
-
+        hmm = BaumWelch(kmeans, holder)
     # Identify peak state as the state with the highest short read signal.
-        # Identify flanking nucleosome state as the state witht the second highest mono read signal.
+    # Identify flanking nucleosome state as the state witht the second highest mono read signal.
+    peak = -1
+    pmax = 0
+    for i in hmm.nbStates:
+        pdf = OpdfMultiGaussian(hmm.getOpdf(i))
+        sh = pdf.mean()
+        if sh > pmax:
+            peak = i
+            pmax = sh
+
+    pmax = 0
+    for i in hmm.nbStates:
+        if i != peak:
+            pdf = OpdfMultiGaussian(hmm.getOpdf(i))
+            mono = pdf.mean()
+            if mono > pmax:
+                pmax = mono
 
     # Output binary model file
-    
+    outModel = open( modelOnly, "w" )
+    HmmBinaryWriter.write(outModel, hmm)  
+    outModel = outModel.write(hmm)
+    outModel.close()
+    info("Model created and refined. See %s.model", output)
+    info("Model: \n %s", hmm)
+
     # Stop program if only model is desired  
+    if modelOnly:
+        info("HMM Model generated. This can be later applied with `--model`")
+        sys.exit(0)
 
     # Split the genome file into smaller 25MB chunks 
-		#  * Can also split into whatever sized chunks the users prefers
-		#  * May be necessary to split into smaller chunks for machines with less memory
+	# Can also split into whatever sized chunks the users prefers
+	# May be necessary to split into smaller chunks for machines with less memory
+    split = SplitBed(genomeStats, window)
 
     # Subtract excluded regions from the split genome for Viterbi
+    vitBed = SubtractBed(split, exclude)
+    info("Genome split and subtracted masked regions...")
 
     # Run Viterbi on the whole genome 
+    if printHMMRTracks: # is print_train = printHMMRTracks?
+        pass # should NFR, mono, di, tri files be included in macs3 file?
+
+    genomeAnnotation = []
+    for i in range(0, len(vitBed)):
+        tempBed = []
+        if vitBed[i] >= 10:
+            tempBed.append(vitBed[i])
+            vGen = FragPileupGen(bamfile, index, tempBed, mode, em_means, em_stddev, minMapQ, keep_dups, cpmScale)
+            vHolder = TrackHolder(vGen, trim)
+            if printHMMRTracks:
+                tracks = HMMRTracksToBedgraph(vHolder, vitBed)
+                pass #nfr, mono, di, tri
+
+            HMM = RobustHMM(vHolder, hmm)
+            hmm_states = HMM.getStates # is this the same as "states"
+            start = vitBed[i].getStart
+            remainder = vitBed[i].getLength % 10
+            pile = []
+            for j in range(0, len(hmm_states)-1):
+                pNode = PileupNode2(start+(j*10), hmm_states, vitBed[i])
+                pile.append(pNode)
+            pNode = PileupNode2((start+((j*10)-remainder)),hmm_states, vitBed[i])
+            pile.append(pNode)
+            genomeAnnotation.append(PileupToBedGraph(pile).getBedGraph)
+
+            if (i % 50 == 0 or i == len(vitBed)-1):
+                info("%d round viterbi done", i)
+    if printHMMRTracks:
+        pass # close write files
 
     # Report the final results as peaks, bedgraphs and summits, if desired
-
+    if storebdg:
+        pass
+    if store_peaks:
+        pass
+    if
         # Execute the scoring commands if the state is a peak or if bedgraph scoring is on
 
         # report the bedgraph if desired
