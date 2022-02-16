bd7067fe6def287457180c3a7d4e87a09c892c9f
Tao Liu <vladimir.liu@gmail.com>
2022-02-16 14:27:18 -0500

    typos and other mistakes.

--- MACS3/Signal/HMMR_EM.pyx
+++ MACS3/Signal/HMMR_EM.pyx
@@ -1,6 +1,6 @@
 # cython: language_level=3
 # cython: profile=True
-# Time-stamp: <2022-02-16 13:58:11 Tao Liu>
+# Time-stamp: <2022-02-16 14:17:03 Tao Liu>
 
 """Module description:
 
@@ -22,7 +22,7 @@ cdef inline float get_weighted_density( x, mean, stddev, weights ):
     return weight * pnorm( x, mean, stddev )
     # return np.multiply(weight, normal_dist_density) # make sure both are np.array types
 
-cdef return_greater( self, list data ):
+cdef int return_greater( list data ):
     """
     Return the index of the largest value in an array of doubles
     @param data: an Array of doubles 
@@ -100,9 +100,9 @@ cdef class HMMR_EM:
         cutoff3 = em_means[ 3 ] - em_means[ 2 ]/2 + em_means[ 2 ]
         
         sum4 = len( self.data )
-        sum3 = sumn( self.data < cutoff3 )
-        sum2 = sumn( self.data < cutoff2 )
-        sum1 = sumn( self.data < cutoff1 )
+        sum3 = sum( self.data < cutoff3 )
+        sum2 = sum( self.data < cutoff2 )
+        sum1 = sum( self.data < cutoff1 )
 
         counter4 = sum4 - sum3
         counter3 = sum3 - sum2
@@ -110,8 +110,7 @@ cdef class HMMR_EM:
         counter1 = sum1
 
         self.weight = [ counter1/sum4, counter2/sum4, counter3/sum4, counter4/sum4]
-
-        self.__learn()
+        self.learn()
         #self.__getMeans()
         #self.__getLambda()
 	# double[] tempMeans = em.getMeans();
@@ -130,130 +129,7 @@ cdef class HMMR_EM:
         # fragMeans and fragStddevs.
         return
 
-
-    #cdef void __pullLargeLengths(self, object bamfile, options.min_map_quality, GENOME, options.em_means):
-        # TL: I will try to implement this
-# public class pullLargeLengths {
-	
-# 	private File bam;
-# 	private File index;
-# 	private int minQ;
-# 	private double[] lengths;
-# 	private ArrayList<TagNode> genome;
-# 	private double[] weights = new double[3];
-# 	private double[] means;
-# 	/**
-# 	 * Constructor for creating pullLargeLengths object, reading the data and setting weights
-# 	 * @param b a File representing the BAM file of reads
-# 	 * @param i a File representing the BAM index file of reads
-# 	 * @param m an integer representing the minimum mapping quality of reads to pull
-# 	 * @param g an ArrayList of TagNode representing the genome
-# 	 * @param mu an Array of doubles representing the means of the fragment length distributions
-# 	 */
-# 	public pullLargeLengths(File b, File i,int m,ArrayList<TagNode> g,double[] mu){
-# 		bam = b;
-# 		index = i;
-# 		minQ = m;
-# 		genome = g;
-# 		means = mu;
-# 		read();
-# 		setWeights();
-# 	}
-# 	/**
-# 	 * Access the lengths
-# 	 * @return an Array of doubles representing the lengths of the fragments read
-# 	 */
-# 	public double[] getLengths(){return lengths;}
-# 	/**
-# 	 * Access the weights
-# 	 * @return an Array of doubles representing the proportion of fragments in each length category
-# 	 */
-# 	public double[] getWeights(){return weights;}
-# 	/**
-# 	 * Access the down sampled weights
-# 	 * @param div an integer representing the factor to divide the number of lengths by to downsample
-# 	 * @return an Array of doubles representing the downsampled lengths
-# 	 */
-# 	public double[] getSampledLengths(int div){
-# 		int len = lengths.length/div;
-# 		double[] newLengths = new double[len];
-# 		shuffle(lengths);
-# 		for(int i = 0; i < len;i++){
-# 			newLengths[i] = lengths[i];
-# 		}
-# 		return newLengths;
-# 	}
-# 	/**
-# 	 * Shuffle the length data
-# 	 * @param list an Array of doubles representing the pulled fragment lengths
-# 	 */
-# 	private void shuffle(double[] list){
-# 		Random rnd = new Random();
-# 		for(int i = list.length -1;i > 0;i--){
-# 			int index = rnd.nextInt(i+1);
-# 			double a = list[index];
-# 			list[index] = list[i];
-# 			list[i] = a;
-# 		}
-# 	}
-# 	/**
-# 	 * Read the data and create a list of lengths
-# 	 */
-# 	private void read(){
-# 		int counter = 0;
-# 		SAMFileReader reader = new SAMFileReader(bam,index);
-# 		ArrayList<Double> temp = new ArrayList<Double>();
-# 		for (int i = 0; i < genome.size();i++){
-# 			String chr = genome.get(i).getChrom();
-# 			int start = genome.get(i).getStart();
-# 			int stop = genome.get(i).getStop();
-# 			CloseableIterator<SAMRecord> iter = reader.query(chr,start,stop,false);
-# 			while (iter.hasNext()){
-# 				SAMRecord record = null;
-# 				try{
-# 					record = iter.next();
-# 				}
-# 				catch(SAMFormatException ex){
-# 					System.out.println("SAM Record is problematic. Has mapQ != 0 for unmapped read. Will continue anyway");
-# 				}
-# 				if(record != null){
-# 				if(!record.getReadUnmappedFlag() && !record.getMateUnmappedFlag() && record.getFirstOfPairFlag()) {
-# 					if (record.getMappingQuality() >= minQ){
-						
-# 						if (Math.abs(record.getInferredInsertSize()) > 100 && Math.abs(record.getInferredInsertSize())
-# 								< 1000){
-# 							counter+=1;
-# 							temp.add((double)Math.abs(record.getInferredInsertSize()));
-# 						}
-# 					}
-# 				}
-# 				}
-# 			}
-# 			iter.close();
-# 		}
-# 		reader.close();
-# 		lengths = new double[counter];
-# 		for (int i = 0;i < temp.size();i++){
-# 			if (temp.get(i) > 100){
-# 				lengths[i] = temp.get(i);
-# 			}
-# 		}
-		
-# 	}
-# 	/**
-# 	 * Set the weights ie the proportion of fragments in each length category
-# 	 */
-# 	private void setWeights(){
-# 		double cutone = (means[1] - means[0])/2 + means[0];
-# 		double cuttwo = (means[2] - means[1])/2 + means[1];
-# 		int counter1=0;
-# 		int counter2=0;
-# 		int counter3=0;
-# 		for (int i = 0;i < lengths.length;i++){
-# 			if (lengths[i] < cutone)
-# 				counter1++;
-
-    cdef __iterate(self):
+    cdef void __iterate(self):
         """Description: This is a private function only used by HMMR_EM class
 
         parameters:
