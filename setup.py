b32f6f4a22a480b198e87fefa7516123a057ec25
Tao Liu <vladimir.liu@gmail.com>
2022-03-10 11:09:31 -0500

    Require python>=3.8

--- setup.py
+++ setup.py
@@ -30,8 +30,6 @@ classifiers =[\
               'Operating System :: POSIX',
               'Operating System :: Unix',
               'Topic :: Scientific/Engineering :: Bio-Informatics',
-              'Programming Language :: Python :: 3.6',
-              'Programming Language :: Python :: 3.7',
               'Programming Language :: Python :: 3.8',
               'Programming Language :: Python :: 3.9',
               'Programming Language :: Python :: 3.10',
