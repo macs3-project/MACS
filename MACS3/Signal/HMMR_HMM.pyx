ced8fdc3f5d519fd39f04a51f00511a57f877b1e
philippadoherty <94635687+philippadoherty@users.noreply.github.com>
2022-03-24 16:38:28 -0400

    skip kmeans

--- MACS3/Signal/HMMR_HMM.pyx
+++ MACS3/Signal/HMMR_HMM.pyx
@@ -23,9 +23,9 @@ import numpy as np
 cimport numpy as np
 from cpython cimport bool
 from hmmlearn import hmm
-from hmmlearn cimport hmm
-from sklearn.cluster import KMeans 
-from sklearn.cluster cimport KMeans
+# from hmmlearn cimport hmm
+# from sklearn.cluster import KMeans 
+# from sklearn.cluster cimport KMeans
 
 # ------------------------------------
 # MACS3 modules
@@ -56,25 +56,20 @@ cdef inline float get_weighted_density( int x, float m, float v, w ):
 # public functions
 # ------------------------------------
 
-cpdef initial_state_kmeans( list training_data, k = 3 ):
+# cpdef initial_state_kmeans( list training_data, k = 3 ):
     # trial for kmeans:
-    kmeans = KMeans(n_clusters = k, random_state = 0).fit(training_data)
-    labels = kmeans.labels_.tolist()
-    ls = []
-    for i in range (k):
-        ls.append(labels.count(i))
-    initial_state = [x / sum(ls) for x in ls]
-    initial_means = kmeans.cluster_centers_
-    initial_cov = np.cov(initial_means)
-    return initial_state, initial_means, initial_cov
+    # kmeans = KMeans(n_clusters = k, random_state = 0).fit(training_data)
+    # labels = kmeans.labels_.tolist()
+    # ls = []
+    # for i in range (k):
+    #     ls.append(labels.count(i))
+    # initial_state = [x / sum(ls) for x in ls]
+    # initial_means = kmeans.cluster_centers_
+    # initial_cov = np.cov(initial_means)
+    # return initial_state, initial_means, initial_cov
 
-cpdef hmm_training( list training_data, initial_state, initial_means, initial_cov ):
-    # training data should be in array like format: X = np.array([[.5, .3, .1, .1], [.6, .4, 0, 0]])
-    # if we do not want init_prob to be updated through learning, set params = 'tmc' and init_prob = initial_state otherwise it will be overwritten
-    # according to base documentation, if init_prob not stated, it is set to be equally likely for any state (1/ # of components)
-    # if we have other known parameters, we should set these (ie: means_weights, covariance_type etc.)
-    # hmm_model = hmm.GaussianHMM( n_components=3, covariance_type = 'full' )
-    hmm_model = hmm.GaussianHMM( n_components=3, init_prob=initial_state, means_prior=initial_means, covar_prior=initial_cov, params='tmc')
+cpdef hmm_training( list training_data ):
+    hmm_model = hmm.GaussianHMM( n_components=3 )
     hmm_model.fit( training_data )
     return hmm_model
 
