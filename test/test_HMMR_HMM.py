0c09e1a6767523885ad978bf4d3186a8db2b21b7
Tao Liu <vladimir.liu@gmail.com>
2024-02-17 00:54:01 -0500

    unit test for hmm_predict

--- test/test_HMMR_HMM.py
+++ test/test_HMMR_HMM.py
@@ -1,6 +1,6 @@
 import unittest
 import pytest
-from MACS3.Signal.HMMR_HMM import hmm_training, hmm_predict
+from MACS3.Signal.HMMR_HMM import *
 import numpy as np
 import numpy.testing as npt
 
@@ -13,12 +13,13 @@ import numpy.testing as npt
 # @pytest.mark.skip(reason="need to refine later")
 class Test_HMM_train(unittest.TestCase):
     def setUp( self ):
-        self.train_data = np.loadtxt("test/large_training_data.txt", delimiter="\t", dtype="float", usecols=(2,3,4,5)).tolist()
+        self.training_data = np.loadtxt("test/large_training_data.txt", delimiter="\t", dtype="float", usecols=(2,3,4,5)).tolist()
         self.training_data_lengths = np.loadtxt('test/large_training_lengths.txt', dtype="int").tolist()
         self.expected_converged = True
         self.not_expected_covars = None
         self.not_expected_means = None
         self.not_expected_transmat = None
+        self.startprob = [0.018070155066844208, 0.9015372740274981, 0.08039257090565771]
         self.means = [[2.05560411e-01, 1.52959594e+00, 1.73568556e+00, 1.00019720e-04],
                       [1.84467806e-01, 1.46784946e+00, 1.67895745e+00, 1.00016654e-04],
                       [2.06402305e+00, 8.60140461e+00, 7.22907032e+00, 1.00847661e-04]]
@@ -39,27 +40,36 @@ class Test_HMM_train(unittest.TestCase):
         self.transmat = [[1.91958645e-03, 9.68166646e-01, 2.99137676e-02],
                          [8.52453717e-01, 1.46924953e-01, 6.21329356e-04],
                          [2.15432113e-02, 6.80080650e-05, 9.78388781e-01]]
-        # self.predictions = np.loadtxt("test/standard_results_hmmratac/predictions.txt", delimiter="\t", dtype="float").tolist()
-        # self.cr_data = np.loadtxt("test/standard_results_hmmratac/cr_data.txt", delimiter="\t", dtype="float").tolist()
-        # self.cr_data_lengths = np.loadtxt("test/standard_results_hmmratac/cr_data_lengths.txt", dtype="float").astype(np.int64).tolist()
+        self.n_features = 4
 
+        # for prediction
+        self.prediction_data = np.loadtxt("test/small_prediction_data.txt", delimiter="\t", dtype="float", usecols=(2,3,4,5)).tolist()
+        self.prediction_data_lengths = np.loadtxt('test/small_prediction_lengths.txt', dtype="int").tolist()
+        self.predictions = np.loadtxt('test/small_prediction_results.txt', delimiter="\t", dtype="float").tolist()
 
-
-    def test_predict( self ):
-        model = hmm_training(training_data = self.train_data, training_data_lengths = self.training_data_lengths, n_states = 3, random_seed = 12345)
-
-        # predictions = hmm_predict(self.cr_data, self.cr_data_lengths, model)
-        # predictions = hmm_predict(self.train_data, self.training_data_lengths, model)
-        # np.savetxt('test/standard_results_hmmratac/predictions.txt', predictions, delimiter="\t", fmt="%f")
-
-        # test training:
+    def test_training( self ):
+        # test hmm_training:
+        model = hmm_training(training_data = self.training_data, training_data_lengths = self.training_data_lengths, n_states = 3, random_seed = 12345, covar = 'full')
         self.assertEqual( model.monitor_.converged, self.expected_converged )
         self.assertNotEqual( model.covars_.tolist(), self.not_expected_covars )
         self.assertNotEqual( model.means_.tolist(), self.not_expected_means )
         self.assertNotEqual( model.transmat_.tolist(), self.not_expected_transmat )
+        npt.assert_allclose( model.startprob_.tolist(), self.startprob )
         npt.assert_allclose(model.means_, self.means)
         npt.assert_allclose(model.covars_, self.covars)
         npt.assert_allclose(model.transmat_, self.transmat)
-        
-        # test predicition:
-        # npt.assert_allclose(predictions, self.predictions) #adjust rtol, atol for tolerance
+        npt.assert_allclose(model.n_features, self.n_features)
+
+    def test_predict( self ):
+        # test hmm_predict
+        hmm_model = hmm.GaussianHMM( n_components=3, covariance_type='full' )
+        hmm_model.startprob_ = np.array(self.startprob)
+        hmm_model.transmat_ = np.array(self.transmat)
+        hmm_model.means_ = np.array(self.means)
+        hmm_model.covars_ = np.array(self.covars)
+        hmm_model.covariance_type = 'full'
+        hmm_model.n_features = self.n_features
+
+        predictions = hmm_predict( self.prediction_data, self.prediction_data_lengths, hmm_model )
+        npt.assert_allclose( predictions, self.predictions )
+
